3944a35beaa670e888c5864f7e64b01304b9afd5
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-01-12 19:12:05 +0100

    Refactor test

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
 
+mongoose.connect('mongodb://localhost/news');
+
 require('./models/Posts');
 require('./models/Comments');
 require('./models/Users');
+
 require('./config/passport');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index');
 
 var app = express();
 
@@ -28,10 +30,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(passport.initialize());
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -50,7 +52,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-mongoose.connect('mongodb://localhost/news');
 
 module.exports = app;
--- models/Posts.js
+++ models/Posts.js
@@ -1,15 +1,17 @@
+
 var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
-    title: String,
-    link: String,
-    upvotes: {type: Number, default: 0},
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+  title: String,
+  link: String,
+  upvotes: {type: Number, default: 0},
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+  author: String
 });
 
-PostSchema.methods.upvote = function(callback) {
-    this.upvotes += 1;
-    this.save(callback);
+PostSchema.methods.upvote = function(cb) {
+  this.upvotes += 1;
+  this.save(cb);
 };
 
 mongoose.model('Post', PostSchema);
